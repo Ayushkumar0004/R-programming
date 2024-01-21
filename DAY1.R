19973ecdf89467fac5aecb49d8d920e8aad37afc
ayush_0001 <ayushk0004@gmail.com>
2024-01-21 11:15:51 +0530

    Intro r programming

---
+++ DAY1.R
@@ -0,0 +1,27 @@
+a <- "hello world"
+print(a)
+b <- "This is AYUSH"
+print(b)
+c = "I'm from Haryana"
+print(c)
+d = "Current I'm purse btech Cse in 4 semester"
+print(d)
+a1 = TRUE
+print(class(a1))
+a2 = FALSE
+print(class(a2))
+b1 = 15.4
+print(class(b1))
+print(typeof(b1))
+b2 = 546
+print(class(b2))
+b3 = 15.6
+print(class(b1))
+c1 = "Hey bro"
+print(class(c1))
+c2 = 2+4i
+print(class(c2))
+c3 = 558L
+print(class(c3))
+"hello" = a
+print(a)
