35a811d5898bdcb726f303463da94141a84177ff
ayush_0001 <ayushk0004@gmail.com>
2024-01-29 12:38:01 +0530

    day3 r programming

---
+++ day3.r
@@ -0,0 +1,88 @@
+"Hello world"
+a <- "Hello World"
+age <- 20
+print(age)
+print(a)
+b <- TRUE
+print(class(b))
+c <- 10.5
+print(class(c))
+print(typeof(c))
+d=6+3i
+print(class(d))
+var_name="Anurag"
+print(var_name)
+var.name="Dhull"
+print(var.name)
+
+g="Hello"
+print(g)
+h <- "helllo"
+print(h)
+"Heooo" ->i
+print(i)
+
+print(ls())
+ls(all.names =TRUE)
+ad=readline("Enter your name")
+gd =as.numeric(readline("enter ur age"))
+
+a1 <-  as.numeric(10L)
+print(a2)
+
+a2 <- as.numeric("Hello ")
+print(a2)
+
+a3 <- as.character("heyyy")
+print(a3)
+
+a4 <- as.numeric(TRUE)
+print(a4)
+
+a5 <- as.integer(10 + 2i)
+print(a5)
+
+x <-10
+y <-20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%%y)
+print(x^y)
+
+
+
+print(a+b)
+print(a-b)
+
+a <-10
+b <-20
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+
+
+i=1
+j=0
+ 
+print(i || j)
+print(i && j)
+print(i | j)
+print(i & j)
+
+a <<- 5
+c(1,2) ->> a
+
+a=10
+b=15
+print(a:b)
+
+
+a <- c(2,3,4)
+b <- c(1,4,5)
+d=2
+print(d %in% a)
