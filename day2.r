dd96c623ce639135963cf5145b360d83206868e3
ayush_0001 <ayushk0004@gmail.com>
2024-01-29 12:37:06 +0530

    day2 r programming

---
+++ day2.r
@@ -0,0 +1,8 @@
+a <- as.numeric(readline("Enter the first number: "))
+print(a)
+a1 <- as.numeric(readline("Enter the second number: "))
+print(a1)
+a2 <- as.numeric(readline("Enter the third number: "))
+print(a2)
+total_sum <- a + a1 + a2
+print(total_sum)
